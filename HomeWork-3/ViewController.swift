3e926a3446e24bd71105125736dba1c34d62361d
Vlad <mackva34@gmail.com>
2024-02-03 21:58:57 +0100

    creating color change logic

--- HomeWork-3/ViewController.swift
+++ HomeWork-3/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        
+        setupLabel()
+        setupSlider()
     }
 
-
+    @IBAction func actionRedSlider() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        colorMixed()
+    }
+    
+    @IBAction func actionGreenSlider() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        colorMixed()
+    }
+    
+    @IBAction func actionBlueSlider() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        colorMixed()
+    }
+    
+    private func colorMixed() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+    
+    private func setupLabel() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func setupSlider() {
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+    }
 }
-
